03733d9a9c9241cee1794dff75114afcdb720b90
Dat Tien <dattien@commercers.com>
2025-06-03 13:15:57 +0700

    [DEV-0603] uptodate 2:08:18

---
+++ client/eslint.config.mjs
@@ -0,0 +1,59 @@
+import { FlatCompat } from "@eslint/eslintrc"
+
+const compat = new FlatCompat({
+    baseDirectory: import.meta.dirname,
+})
+
+const eslintConfig = [
+    // 👇 Ignorierte Dateien/Folders (MUSS ein eigenes Objekt ohne andere Keys sein!)
+    {
+        ignores: [
+            "**/.next",
+            "**/next-env.d.ts",
+            "**/node_modules",
+            "**/yarn.lock",
+            "**/package-lock.json",
+            "**/public",
+            "**/src/types",
+        ],
+    },
+
+    // 👇 Dein eigentlicher ESLint-Setup
+    ...compat.config({
+        extends: [
+            "next",
+            "next/core-web-vitals",
+            "next/typescript",
+            "plugin:prettier/recommended",
+            "plugin:jsx-a11y/recommended",
+        ],
+        plugins: ["prettier", "jsx-a11y"],
+        rules: {
+            "prettier/prettier": [
+                "error",
+                {
+                    trailingComma: "all",
+                    semi: false,
+                    tabWidth: 4,
+                    singleQuote: false,
+                    printWidth: 80,
+                    endOfLine: "auto",
+                    arrowParens: "always",
+                    plugins: ["prettier-plugin-tailwindcss"],
+                },
+                {
+                    usePrettierrc: false,
+                },
+            ],
+            "react/react-in-jsx-scope": "off",
+            "jsx-a11y/alt-text": "warn",
+            "jsx-a11y/aria-props": "warn",
+            "jsx-a11y/aria-proptypes": "warn",
+            "jsx-a11y/aria-unsupported-elements": "warn",
+            "jsx-a11y/role-has-required-aria-props": "warn",
+            "jsx-a11y/role-supports-aria-props": "warn",
+        },
+    }),
+]
+
+export default eslintConfig
